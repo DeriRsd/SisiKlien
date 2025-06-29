200f17efcd4c335d1185cc105fa72f528b0bca37
DeriRsd <cepderirosadi@gmail.com>
2025-06-29 15:57:32 +0700

    fix: use final production API endpoint with https

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
+  // Pastikan menggunakan https dan URL yang benar
   baseURL: "https://my-json-server.typicode.com/DeriRsd/SisiKlien",
   headers: {
     "Content-Type": "application/json",
